36cb0f0464dceaf19078d205354004acfbe219f3
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-21 10:25:08 +0300

    Nodejs Başlangıç Projesi | BASE API | Api Güvenliği - INJECTION | ( 8 / 12 )

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const router = require('./src/routers');
 const errorHandlerMiddleware = require('./src/middlewares/errorHandler');
 const cors = require('cors');
 const corsOptions = require("./src/helpers/corsOptions")
+const mongoSanitize = require('express-mongo-sanitize');
 
 // Middlewares
 app.use(express.json());
@@ -16,6 +17,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 app.use(cors(corsOptions))
+
+app.use(
+    mongoSanitize({
+      replaceWith: '_',
+    }),
+);
 
 app.use('/api', router);
 
