b88012acbb88d76bb85c1f22ef317e3e083d479d
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-21 16:03:55 +0300

    Nodejs| BASE API | Express Rate Limit - Api Güvenliği | ( 10 / 12 )

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const cors = require('cors');
 const corsOptions = require("./src/helpers/corsOptions")
 const mongoSanitize = require('express-mongo-sanitize');
 const path = require("path")
+const apiLimiter = require("./src/middlewares/rateLimit")
 
 // Middlewares
 app.use(express.json());
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use("/uploads", express.static(__dirname))
 
 app.use(cors(corsOptions))
+
+app.use("/api", apiLimiter)
 
 app.use(
     mongoSanitize({
