764a149aa8364670bf03ae855b4a251be288af63
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-21 10:15:18 +0300

    Nodejs Başlangıç Projesi | BASE API | CORS - URL ERİŞİM İZİNLERİ | ( 7 / 12 )

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-require("express-async-errors")
-const express = require("express")
-const app = express()
-require("dotenv").config()
-require("./src/db/dbConnection")
-const port = process.env.PORT || 5001
-const router = require("./src/routers")
-const errorHandlerMiddleware = require("./src/middlewares/errorHandler")
+require('express-async-errors');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+require('./src/db/dbConnection');
+
+const port = process.env.PORT || 5001;
+const router = require('./src/routers');
+const errorHandlerMiddleware = require('./src/middlewares/errorHandler');
+const cors = require('cors');
+const corsOptions = require("./src/helpers/corsOptions")
 
 // Middlewares
-app.use(express.json())
-app.use(express.json({limit: "50mb"}))
-app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}))
+app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
+app.use(cors(corsOptions))
 
+app.use('/api', router);
 
-app.use("/api", router)
-
-
-
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hoş Geldiniz"
-    })
-})
-
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hoş Geldiniz',
+  });
+});
 
 // hata yakalama
-app.use(errorHandlerMiddleware)
+app.use(errorHandlerMiddleware);
 
 app.listen(port, () => {
-    console.log(`Server ${port} portundan çalışıyor ...`);
-})
+  console.log(`Server ${port} portundan çalışıyor ...`);
+});
